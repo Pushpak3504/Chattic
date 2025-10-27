1dee1b1a14b3bb7702a58cbb81f9fd55a618d5bb
Pushpak <154728724+Pushpak3504@users.noreply.github.com>
2025-10-27 08:09:05 +0530

    Update2

--- miniio.sh
+++ miniio.sh
@@ -4,10 +4,11 @@ wget https://dl.min.io/server/minio/release/linux-amd64/minio -O minio
 chmod +x minio 
 sudo mv minio /usr/local/bin/
 mkdir ~/minio-data
-
 minio server ~/minio-data --console-address ":9001"
 
 #Client
 #wget https://dl.min.io/client/mc/release/linux-amd64/mc -O mc
 #chmod +x mc
 #sudo mv mc /usr/local/bin/
+#mc alias set local http://localhost:9000 <ACCESS_KEY> <SECRET_KEY>
+#mc ls local
